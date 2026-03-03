c0041f68aeb0d51b689b37398918a90594255cf4
ClaraFranchello <claraguifran@gmail.com>
2026-03-03 16:44:28 -0300

    Add files via upload

---
+++ info26 Clara/ex05.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  float valor;
+    printf("Valor: ");
+    scanf("%f", &valor);
+
+    if(valor > 0){
+      printf("- Positivo!");
+    }
+    else if(valor >= -1){
+
+      printf("- Negativo!");
+    }
+    else{
+      printf("- Neutro");
+    }
+  
+    return 0;
+}
---
+++ info26 Clara/ex10.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+int main(){
+  int a, b;
+  int dia, mes;
+  printf("Digite o dia e mes: ");
+  scanf("%i %i", &dia, &mes);
+  if (dia <= 31 ){ 
+  
+    if (mes == 12){
+      printf("data = %i/dez", dia); 
+    }
+    else if (mes == 11){
+     if(dia > 30){
+        printf("\nDia inválido!");
+      } 
+      else if (dia <= 30){
+        printf("data = %i/nov", dia);
+      }
+    }
+    else if (mes == 10){
+      printf("data = %i/out", dia); 
+    }
+    else if (mes == 9){
+      if(dia > 30){
+        printf("\nDia inválido!");
+      } 
+      else if (dia <= 30){
+        printf("data = %i/set", dia);
+      }
+    }
+    else if (mes == 8){
+      printf("data = %i/ago", dia); 
+    }
+    else if (mes == 7){
+      printf("data = %i/jul", dia); 
+    }
+    else if (mes == 6){
+     if(dia > 30){
+        printf("\nDia inválido!");
+      } 
+      else if (dia < 30){
+        printf("data = %i/jun", dia);
+      }
+    }
+    else if (mes == 5){
+      printf("data = %i/mai", dia); 
+    }
+    else if (mes == 4){
+      if(dia > 30){
+        printf("\nDia inválido!");
+      } 
+      else if (dia <= 30){
+        printf("data = %i/abr", dia);
+      }
+    }
+    else if (mes == 3){
+      printf("data = %i/mar", dia);
+    }
+    else if (mes == 2){
+      if(dia > 28){
+        printf("\nDia inválido!");
+      } 
+      else if (dia <= 28){
+        printf("data = %i/fev", dia);
+      }
+    }
+    else if (mes == 1){
+      printf("data:  %i/jan", dia); 
+    }
+    else if (mes > 12){
+      printf("- Mes invalido! ");
+    } 
+  }
+  else if(dia > 31 ){
+    printf("- Dia invalido! ");
+  }
+  
+  
+ 
+
+  return 0;
+}
+   
